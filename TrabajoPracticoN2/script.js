16c7642ff3a1e96fdb6f3d34f033b76a72f3efdc
NestorRebo <rebolledonestor@gmail.com>
2023-08-26 15:36:11 -0300

    mejorar el js

--- TrabajoPracticoN2/script.js
+++ TrabajoPracticoN2/script.js
@@ -11,80 +11,58 @@ let eleccionJugador;
 
 function saludarJugador() {
     nombre = document.getElementById("nombreUsuario").value;
-    
+
     if (nombre == "" || !(isNaN(nombre))) {
         document.getElementById("saludoUsuario").innerHTML = "Ingrese su nombre";
     }
     document.getElementById("saludoUsuario").innerHTML = "Hola " + " " + nombre + " " + "empecemos a jugar!!!";
+}
+
+
+function partida(eleccionJugador) {
+    let contadorJugador;
+    let contadorCPU;
+
+    numero = Math.floor(Math.random() * 2);
+    eleccionMaquina = opciones[numero];
+
+
+    if (eleccionJugador == 0 && opciones[eleccionMaquina] == 1 || eleccionJugador == 1 && opciones[eleccionMaquina] == 2 || eleccionJugador == 2 && opciones[eleccionMaquina] == 0) {
+        //gana la compu
+        contadorCPU ++ ;
+    } else if (eleccionJugador == 0 && opciones[eleccionMaquina] == 2 || eleccionJugador == 1 && opciones[eleccionMaquina] == 0 || eleccionJugador == 2 && opciones[eleccionMaquina] == 1) {
+        // gana jugador
+        contadorJugador ++ ;
+    } else {
+        //empate-
+    }
+    
+    if (contadorCPU > 3) {
+        //gana el juego CPU
+    } else if (contadorJugador > 3) {
+        //gana el juego el jugador
+    } else {
+
+    }
 
 
 }
 
 function botonPiedra() {
     eleccionJugador = opciones[0]
-    gana(eleccionJugador)
+    partida(eleccionJugador)
 }
 
 function botonPapel() {
     eleccionJugador = opciones[1]
-    gana(eleccionJugador)
+    partida(eleccionJugador)
 }
 
 function botonTijera() {
     eleccionJugador = opciones[2]
-    gana(eleccionJugador)
+    partida(eleccionJugador)
 }
 
-function aleatorio(minimo, maximo) {
-    let numero = 0;
-    numero = Math.floor(Math.random() * (maximo - minimo + 1) + minimo);
-    return numero;
-}
-
-function jugadaCPU() {
-    numero = aleatorio(0, 2)
-    eleccionMaquina = opciones[numero];
-    return eleccionMaquina
-}
-
-function gana(eleccionJugador) {
-    jugadaCPU();
-    switch (eleccionJugador) {
-        case 0: //el usuario eligio piedra 
-            if (opciones[eleccionMaquina] == 1) {//si la maquina eligio papel 
-                document.getElementById('resultado').innerHTML = '¡Perdiste! La maquina eligio papel y tu piedra.';
-            } else if (opciones[eleccionMaquina] == 2) {
-                document.getElementById('resultado').innerHTML = '¡Ganaste! La maquina eligio tijera y tu piedra.';
-            } else {
-                document.getElementById('resultado').innerHTML = '¡Empate! Ambos eligieron piedra.';
-            }
-            break;
-
-        case 1: //el usuario eligio papel
-            if (opciones[eleccionMaquina] == 2) {
-                document.getElementById('resultado').innerHTML = '¡Perdiste! La maquina eligio tijera y tu papel';
-            } else if (opciones[eleccionMaquina] == 0) {
-                document.getElementById('resultado').innerHTML = '¡Ganaste! La maquina eligio piedra y tu papel';
-            } else {
-                document.getElementById('resultado').innerHTML = '¡Empate! Ambos eligieron papel.';
-            }
-            break;
-
-        case 2: //el usuario eligio tijera 
-            if (opciones[eleccionMaquina] == 1) {
-                document.getElementById('resultado').innerHTML = '¡Ganaste! La maquina eligio papel y tu tijera';
-
-            } else if (opciones[eleccionMaquina] == 0) {
-                document.getElementById('resultado').innerHTML = '¡Perdiste! La maquina eligio piedra y tu tijera.';
-            } else {
-                document.getElementById('resultado').innerHTML = '¡Empate! Ambos eligieron tijera.';
-            }
-            break;
-
-        default:
-            console.log("no ha elegido nada")
-    }
-}
 
 document.addEventListener("DOMContentLoaded", function () {
     //llama a todas las funciones
