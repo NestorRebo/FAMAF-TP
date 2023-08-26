5c6de0483a1df45c66d693128ec1b8380d25342b
NestorRebo <rebolledonestor@gmail.com>
2023-08-26 15:05:51 -0300

    nuevas imagenes y mejora de js

--- TrabajoPracticoN2/script.js
+++ TrabajoPracticoN2/script.js
@@ -1,33 +1,42 @@
-//variables globales para evitar
+//variables globales
 let nombre;
-let numero;
+
 // opciones = ["piedra", "papel", "tijera"];
 let opciones = [0, 1, 2];
 let eleccionMaquina;
 let eleccionJugador;
 
 //funciones
+
+
 function saludarJugador() {
     nombre = document.getElementById("nombreUsuario").value;
+    
+    if (nombre == "" || !(isNaN(nombre))) {
+        document.getElementById("saludoUsuario").innerHTML = "Ingrese su nombre";
+    }
     document.getElementById("saludoUsuario").innerHTML = "Hola " + " " + nombre + " " + "empecemos a jugar!!!";
+
+
 }
 
 function botonPiedra() {
     eleccionJugador = opciones[0]
-    gana(eleccionJugador, eleccionMaquina)
+    gana(eleccionJugador)
 }
 
 function botonPapel() {
     eleccionJugador = opciones[1]
-    gana(eleccionJugador, eleccionMaquina)
+    gana(eleccionJugador)
 }
 
 function botonTijera() {
     eleccionJugador = opciones[2]
-    gana(eleccionJugador, eleccionMaquina)
+    gana(eleccionJugador)
 }
 
 function aleatorio(minimo, maximo) {
+    let numero = 0;
     numero = Math.floor(Math.random() * (maximo - minimo + 1) + minimo);
     return numero;
 }
@@ -35,46 +44,47 @@ function aleatorio(minimo, maximo) {
 function jugadaCPU() {
     numero = aleatorio(0, 2)
     eleccionMaquina = opciones[numero];
+    return eleccionMaquina
 }
 
-function gana(eleccionJugador, eleccionMaquina) {
-    if (eleccionJugador == 0) {//el usuario eligio piedra 
-        if (opciones[eleccionMaquina] == 1) {//si la maquina eligio papel 
-            document.getElementById('resultado').innerHTML = '¡Perdiste! La maquina eligio papel y tu piedra.';
-        } else if (opciones[eleccionMaquina] == 2) {
-            document.getElementById('resultado').innerHTML = '¡Ganaste! La maquina eligio tijera y tu piedra.';
-        } else {
-            document.getElementById('resultado').innerHTML = '¡Empate! Ambos eligieron piedra.';
-        }
-    }
+function gana(eleccionJugador) {
+    jugadaCPU();
+    switch (eleccionJugador) {
+        case 0: //el usuario eligio piedra 
+            if (opciones[eleccionMaquina] == 1) {//si la maquina eligio papel 
+                document.getElementById('resultado').innerHTML = '¡Perdiste! La maquina eligio papel y tu piedra.';
+            } else if (opciones[eleccionMaquina] == 2) {
+                document.getElementById('resultado').innerHTML = '¡Ganaste! La maquina eligio tijera y tu piedra.';
+            } else {
+                document.getElementById('resultado').innerHTML = '¡Empate! Ambos eligieron piedra.';
+            }
+            break;
 
-    if (eleccionJugador == 1) {//el usuario eligio papel 
-        if (opciones[eleccionMaquina] == 2) {
-            document.getElementById('resultado').innerHTML = '¡Perdiste! La maquina eligio tijera y tu papel';
-        } else if (opciones[eleccionMaquina] == 0) {
-            document.getElementById('resultado').innerHTML = '¡Ganaste! La maquina eligio piedra y tu papel';
+        case 1: //el usuario eligio papel
+            if (opciones[eleccionMaquina] == 2) {
+                document.getElementById('resultado').innerHTML = '¡Perdiste! La maquina eligio tijera y tu papel';
+            } else if (opciones[eleccionMaquina] == 0) {
+                document.getElementById('resultado').innerHTML = '¡Ganaste! La maquina eligio piedra y tu papel';
+            } else {
+                document.getElementById('resultado').innerHTML = '¡Empate! Ambos eligieron papel.';
+            }
+            break;
 
-        } else {
-            document.getElementById('resultado').innerHTML = '¡Empate! Ambos eligieron papel.';
-        }
-    }
+        case 2: //el usuario eligio tijera 
+            if (opciones[eleccionMaquina] == 1) {
+                document.getElementById('resultado').innerHTML = '¡Ganaste! La maquina eligio papel y tu tijera';
 
+            } else if (opciones[eleccionMaquina] == 0) {
+                document.getElementById('resultado').innerHTML = '¡Perdiste! La maquina eligio piedra y tu tijera.';
+            } else {
+                document.getElementById('resultado').innerHTML = '¡Empate! Ambos eligieron tijera.';
+            }
+            break;
 
-    if (eleccionJugador == 2) {//el usuario eligio tijera 
-        if (opciones[eleccionMaquina] == 1) {
-            document.getElementById('resultado').innerHTML = '¡Ganaste! La maquina eligio papel y tu tijera';
-
-        } else if (opciones[eleccionMaquina] == 0) {
-            document.getElementById('resultado').innerHTML = '¡Perdiste! La maquina eligio piedra y tu tijera.';
-        } else {
-            document.getElementById('resultado').innerHTML = '¡Empate! Ambos eligieron tijera.';
-        }
+        default:
+            console.log("no ha elegido nada")
     }
 }
-
-
-
-
 
 document.addEventListener("DOMContentLoaded", function () {
     //llama a todas las funciones
